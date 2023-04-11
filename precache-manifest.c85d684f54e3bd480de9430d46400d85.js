c1ede49fca6e68132873d934e6821f345ef0eef5
Code Sandbox <hello@codesandbox.io>
2023-04-11 13:56:29 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.c85d684f54e3bd480de9430d46400d85.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6edacd0a78896d7f89b2aae784831bab",
+    "url": "https://zertini.github.io/csb-r7v6ck/index.html"
+  },
+  {
+    "revision": "b52d5f040794b4ab8c77",
+    "url": "https://zertini.github.io/csb-r7v6ck/static/js/2.b9007b9d.chunk.js"
+  },
+  {
+    "revision": "226f8caf1681be87133b",
+    "url": "https://zertini.github.io/csb-r7v6ck/static/js/main.15fe78fd.chunk.js"
+  },
+  {
+    "revision": "c2f6588ce5779c861d5d",
+    "url": "https://zertini.github.io/csb-r7v6ck/static/js/runtime-main.6ae38f33.js"
+  }
+]);
